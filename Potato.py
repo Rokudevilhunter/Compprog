587e04a7dae90192c38d64b5353c037dbb0e3830
Scholar <scholar@paradigm.local>
2023-08-31 13:29:14 -0600

    .\Potato.py

---
+++ Potato.py
@@ -0,0 +1,24 @@
+Person = "Ryan"
+
+dictionary = {
+    'name': 'ryan'
+}
+
+def is_even_number(number):
+    if number % 2 == 0:
+        print('this is an even number')
+
+    else:
+        print ('this is an odd number')
+
+    is_even_number(43)
+
+
+    MAIN_CHARACTER = "Mario"
+
+    main_character = "luigi"
+
+
+user_name = input('your name is not')
+
+myNameIsNot = user_name
