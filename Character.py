a742a6b2df9fd9511e8cfef2c8aaab70e7d7181d
Scholar <scholar@paradigm.local>
2023-08-31 12:27:18 -0600

    character created

---
+++ Character.py
@@ -0,0 +1,11 @@
+user_name = input('Whats your name')
+user_age = input('whats your age')
+user_sex = input('are you male female or other')
+character = {
+    "Name": user_name,
+    "Age": user_age,
+    "isMale": user_sex,
+    "Fav_food_list": ['watermelon', 'lettuce', 'sandwich'],
+    "isAlive": True,
+    }
+print('i am a',user_age, user_sex, 'named', user_name)
