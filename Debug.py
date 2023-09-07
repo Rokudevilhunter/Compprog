48b209c6b0f079d60bdd238c158a9451efb6ced1
Scholar <scholar@paradigm.local>
2023-09-07 12:46:29 -0600

    .\Debug.py

---
+++ Debug.py
@@ -0,0 +1,9 @@
+greeting = input("Hello, possible pirate! What's the password?")
+
+if greeting in ("Arrr!"):
+
+	print("Go away, pirate.")
+
+if greeting in ('1234'):
+
+    print("Greetings, hater of pirates!")
