eb3042b0f32267a2dd2ec41cca4d7bf0c4d4a622
Scholar <scholar@paradigm.local>
2023-09-07 13:34:43 -0600

    .\Concept.py

---
+++ Concept.py
@@ -0,0 +1,56 @@
+#Scope
+def add_numbers():
+    sum = 5 + 4
+#Local variable
+#Global variable
+my_name = "Ryan"
+#Input
+user_input = input ("What is your name? ")
+#Output
+print('Nice to meet you,' {user_input}!)
+#String
+my_string = "This is my string."
+#Integer
+my_integer = 69
+#Boolean
+is_my_boolean = True
+x = True
+#Float
+my_float = 3.14
+#Declaration
+my_declaration = ""
+#Initialization
+my_initialization = "hello world"
+#Arithmetic operators
+math = ('add', 'subtract', 'multiply', 'divide', 'modulo', 'exponent')
+add = 1 + 2
+subtract = 2 - 1
+multiply = 2 * 1
+divide = 2 / 1
+modulo = 10 % 3
+exponent = 2 ** 3
+print(math)
+#Comparison operators
+statement = 4 > 2
+print(statement)
+#Order of operations
+#Logical operators
+message = 4 > 2 and 33 < 43
+message2 = 4 > 2 or 33 < 43
+message3 = print(not x)
+#Expression
+#Integer division
+#Float division
+#Function
+#Readability
+#Reusability
+#Modularity
+#Abstraction
+#Built-in function
+#User-defined function
+#Arguments
+#Parameters
+#Return
+#Void return
+#Simple data types
+#Complex data types
