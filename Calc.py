04f225edf16a8ec2c7d52de431b693a7f45e8ff0
Scholar <scholar@paradigm.local>
2023-09-14 13:31:44 -0600

    CALC

--- Calc.py
+++ Calc.py
@@ -1,1 +1,25 @@
-print("The Calc ")
+print("The CALC" )
+
+num1 = int(input("What is your first number? "))
+
+math = input("add, subtract, multiply, divide ")
+
+num2 = int(input("What is your second number "))
+
+if math in ["add"]:
+    print("Your answer is")
+    print(num1 + num2)
+
+if math in ["subtract"]:
+    print("Your answer is")
+    print(num1 - num2)
+
+if math in ["multiply"]:
+    print("Your answer is")
+    print(num1 * num2)
+
+if math in ["divide"]:
+    print("Your answer is")
+    print(num1 / num2)
+
+
