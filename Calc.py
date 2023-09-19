d838fca88121cf2afb2a5d75ced75acfe811e69a
Scholar <scholar@paradigm.local>
2023-09-19 13:31:54 -0600

    intro to control

--- Calc.py
+++ Calc.py
@@ -6,19 +6,19 @@ math = input("add, subtract, multiply, divide ")
 
 num2 = int(input("What is your second number "))
 
-if math in ["add"]:
+if math in ["add"] or math in ["Add"]:
     print("Your answer is")
     print(num1 + num2)
 
-if math in ["subtract"]:
+if math in ["subtract"] or math in ["Subtract"]:
     print("Your answer is")
     print(num1 - num2)
 
-if math in ["multiply"]:
+if math in ["multiply"] or math in ["Multiply"]:
     print("Your answer is")
     print(num1 * num2)
 
-if math in ["divide"]:
+if math in ["divide"] or math in ["Divide"]:
     print("Your answer is")
     print(num1 / num2)
 
---
+++ control.py
@@ -0,0 +1,28 @@
+# car = "Blue"
+# car_make = "Tesla"
+
+# if car == "Blue" or car == "blue":
+#     print("My car is blue")
+#     if car_make == "Tesla":
+#         print("My car is a tesla")
+# elif car == "Red" or car == "red":
+#     print("My car is red")
+# else:
+#     print("i just have a bike")
+
+# for i in range(1, 10):
+#     print(i)
+
+# sports = ['soccer', 'basketball', 'hockey', 'football']
+
+# for item in sports:
+#     print(item)
+
+i = 1
+while i < 6:
+    print("i am still going")
+    i += 1
+
+
+
+
