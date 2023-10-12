3beaff0d8725b5a834adaa1ee281426e2cf36e28
Scholar <scholar@paradigm.local>
2023-10-12 13:10:10 -0600

    morsecode

---
+++ Morsecode.py
@@ -0,0 +1,136 @@
+morse = input("enter dots (.) and dashes (-)")
+
+if morse == (".-"):
+    print("a")
+
+if morse == ("-..."):
+    print("b")
+
+if morse == ("-.-."):
+    print("c")
+
+if morse == ("-.."):
+    print("d")
+
+if morse == ("."):
+    print("e")
+
+if morse == ("..-."):
+    print("f")
+
+if morse == ("--."):
+    print("g")
+
+if morse == ("...."):
+    print("h")
+
+if morse == (".."):
+    print("i")
+
+if morse == (".---"):
+    print("j")
+
+if morse == ("-.-"):
+    print("k")
+
+if morse == (".-.."):
+    print("l")
+
+if morse == ("--"):
+    print("m")
+
+if morse == ("-."):
+    print("n")
+
+if morse == ("---"):
+    print("o")
+
+if morse == (".--."):
+    print("p")
+
+if morse == ("--.-"):
+    print("q")
+
+if morse == (".-."):
+    print("r")
+
+if morse == ("..."):
+    print("s")
+
+if morse == ("-"):
+    print("t")
+
+if morse == ("..-"):
+    print("u")
+
+if morse == ("...-"):
+    print("v")
+
+if morse == (".--"):
+    print("w")
+
+if morse == ("-..-"):
+    print("x")
+
+if morse == ("-.--"):
+    print("y")
+
+if morse == ("--.."):
+    print("z")
+
+if morse == (".----"):
+    print("1")
+
+if morse == ("..---"):
+    print("2")
+
+if morse == ("...--"):
+    print("3")
+
+if morse == ("....-"):
+    print("4")
+
+if morse == ("....."):
+    print("5")
+
+if morse == ("-...."):
+    print("6")
+
+if morse == ("--..."):
+    print("7")
+
+if morse == ("---.."):
+    print("8")
+
+if morse == ("----."):
+    print("9")
+
+if morse == ("-----"):
+    print("0")
+
+if morse == ("--..--"):
+    print(",")
+
+if morse == (".-.-.-"):
+    print(".")
+
+if morse == ("..--.."):
+    print("?")
+
+if morse == ("---..."):
+    print(":")
+
+if morse == ("-....-"):
+    print("-")
+
+if morse == ("-..-."):
+    print("/")
+
+if morse == ("-.--."):
+    print("(")
+
+if morse == ("-.--.-"):
+    print(")")
+
+
+
