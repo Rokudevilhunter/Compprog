23153c80224b93f567156ad3dc28eb0c8dd52f44
Scholar <scholar@paradigm.local>
2023-10-31 12:49:33 -0600

    IntroToCareers

---
+++ careers.py
@@ -0,0 +1,26 @@
+Comp_prog_team = {
+"team_leader": {
+    "name": "Jim",
+    "duties": ["supervise", "assist", "advocate"]
+    },
+"analyst": {
+    "name": "Oscar",
+    "duties": ["analyze", "examine", "collect"]
+    },
+"senior_dev": {
+    "name": "Dwight",
+    "duties": ["train", "assist", "develop"]
+    },
+"jr_dev": {
+    "name": "Ryan",
+    "duties": ["learn", "develop"]
+    },
+"client": {
+    "name": "jan",
+    "duties": ["propose", "critique", "request"]
+    },
+"product_manager": {
+    "name": "Michael",
+    "duties": ["manage", "mediate", "advocate"]
+    },
+}
