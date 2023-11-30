7e77384ddb4df6f5980904014ac8e20468bcec77
Scholar <scholar@paradigm.local>
2023-11-30 13:09:50 -0700

    Rpg.py

---
+++ RPG.py
@@ -0,0 +1,27 @@
+## import stuff
+import random
+
+## walk around
+
+w3 = ("you are in a room. Right: (another room) Left: (another room) Front: (Door) Back: (Wall) Up: (Light) Down: (dusty stone floor) ")
+
+
+## game start
+print("GET OUT")
+print("welcome to GET OUT, this game is about a room that is slowly being slowly filled with fear...")
+play = input("would you like to play? y/n    ")
+if play == ("y"):
+    print (w3)
+elif play == ("n"):
+    print("ok, then why are you here?")
+    
+
+
+## inventory
+
+
+## random atk
+
+## wall check
+
+## chest
