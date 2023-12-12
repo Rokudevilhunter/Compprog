4f60686000c7d1ca2dfcc83d46d17f1b069242fb
Rokudevilhunter <Rokudevilhunter@outlook.com>
2023-12-11 21:53:57 -0700

    .\RPG.py

--- RPG.py
+++ RPG.py
@@ -1,121 +1,81 @@
 ## import stuff
 import random
+import time
 
 ## Side stuff
+def playerHealth():
+    print()
+def mobHealth():
+    print()
+def mobRun():
+    print()
 
-
-
-## rooms
-w1 = ("you are in a room. Right: (wall) Left: (Wall) Front: (A chest) Back: (door) Up: (Light) Down: (A nice red carpet) ")
-w2 = ("you are in a room. Right: (another room) Left: (Wall) Front: (painting) Back: (Window) Up: (Light) Down: (dusty stone floor) ")
-w3 = ("you are in a room. Right: (another room) Left: (another room) Front: (Door) Back: (Wall) Up: (Light) Down: (dusty stone floor) ")
-w4 = ("you are in a room. Right: (wall) Left: (Wall) Front: (A chest) Back: (door) Up: (Light) Down: (A nice red carpet) ")
-
-## game start
-print("GET OUT")
-print("welcome to GET OUT, this game is about a room that is slowly being slowly filled with fear...")
-play = input("would you like to play? y/n    ")
-
-if play == ("y"):
-    cr = w3
-    print (cr)
-    act = input("what do you do?    ")
-elif play == ("n"):
-    print("ok, then why are you here?")
+def mobAtk():
+    print("")
+def mobAction():
+    print("do you want to Attack or Run")
     
-## Movment
-if act == ("help"):
-    print("(MOVMENT  (Forward = f) (Backward = b) (left = l) (right = r)  )   (Inventory  (look in inventory = inv)  ) (Attack  (Fight = af) (Run away = ar))") 
+def randMob():
+    mob = random.randint(1,3)
+    if mob == 1:
+        print("Oh No! a snake is slithering your way\n")
+    elif mob == 2:
+        print("Oh No! a large spider dropped from the ceiling\n")
+
+    elif mob == 3:
+        print("Oh a hissing black cat lurks in the shadows!\n")
     
-
-if act == ("f"):
-    if cr == w1:
-        print("You can't walk into a wall!")
-
-
-    elif cr == w2:
-        print("You can't walk into a wall!")
-
-            
-    elif cr == w3:
-        if dl == True:
-            print("uh oh, the door is locked!")
-            
+def newRoom():
+    
+    while True:
+        print("room desc\n")
+        ifmob = random.randint(1,4)
+        if ifmob == (1):
+            randMob()
+            mobAction()
+            break
         else:
-            cr = w1
-            print(cr)
+            action = input('Press "w" to walk to the next room.\n')
+            if action.lower() == ("w"):
+                continue
+                
 
+def game():
+    while True: 
+        print ("welcome to RandomPlayerGame (RPG), where you, a random person, randomly move through a maze and randomly encounter magical beasts!\n")
+        time.sleep(1)
+        gameStart = input("Would you like to play?\n")
+        time.sleep(1)
+        if gameStart.lower() == ("y"):
+            ##inv = []
+            ##health = 10
+            name = input("what shall I call you, random player?\n")
+            print('ok', name)
+            action = input('Welcome to the first room, press "w" to walk to the next room.\n')
+            if action.lower() == ("w"):
+                newRoom()
+            else:
+                break
+            break
+        else:
+            nr = random.randint(1,5)
+            if nr == 1:
+                print("Ha, I knew you were a wimp!\n")
+                break
+            if nr == 2:
+                print("Scram! You are wasting my time!\n")
+                break
+            if nr == 3:
 
-    elif cr == w4: 
-        print("You can't walk into a wall!")
+                print("  >:(  \n")
+                break
+            if nr == 4:
 
+                print("You have Really Poor Gamesmanship (RPG). ;)\n")
+                break
+            if nr == 5:
 
-if act == ("b"):
-    if cr == w1:
-        cr = w2
-        print(cr)
+                print("Thank goodness, I'm glad you're not staying!\n")
+                break
 
-
-    elif cr == w2:
-        print("oh no, the window is barred!")
-
-
-    elif cr == w3:
-        print("although the painting looks interesting, you still can't walk into walls.....or jump into paintings.")
-
-
-    elif cr == w4: 
-        print("you can't walk into walls")
-
-
-if act == ("l"):
-    if cr == w1:
-        print("You can't walk into walls")
-
-
-    if cr == w2:
-        print("you can't walk into walls")
-
-
-    if cr == w3:
-        cr = w2
-        print(cr)
-
-        
-    if cr == w4: 
-        cr = w3
-        print(cr)
-
-
-if act == ("r"):
-    if cr == w1:
-        print("you can't walk into walls")
-
-
-    if cr == w2:
-        cr = w3
-        print(cr)
-
-
-    if cr == w3:
-        cr = w4
-        print(cr)
-
-
-    if cr == w4:
-        print("You can't walk into walls") 
-
-
-
-## inventory
-
-
-## random atk
-atk = random.randint(1,4)
-if atk == 1 and cr == w2:
-    print("atk")
-
-## wall check
-
-## chest
-
+game()
